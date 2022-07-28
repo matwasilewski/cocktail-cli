af3470e83d33c314a23282cd3dcf5206b36419f9
matwasilewski <mat.p.wasilewski@gmail.com>
2022-07-28 12:56:06 +0100

    add requests cache

--- src/cocktail_cli/api.py
+++ src/cocktail_cli/api.py
@@ -1,15 +1,34 @@
 import logging
+from datetime import timedelta
 from typing import List
 
 import requests
+from requests_cache import CachedSession
 
+session = CachedSession(
+    "cocktails_cache",
+    use_cache_dir=True,
+    cache_control=True,
+    expire_after=timedelta(days=1),
+    allowable_methods=[
+        "GET",
+        "POST",
+    ],
+    allowable_codes=[
+        200,
+        400,
+    ],
+    ignored_parameters=["api_key"],
+    match_headers=True,
+    stale_if_error=True,
+)
 cocktail_url = "https://www.thecocktaildb.com/api/json/v1/1/filter.php"
 
 
 def cocktails_with_ingredients(ingredients: List[str]):
     headers = {"Accept": "application/json"}
     payload = {"i": ingredients}
-    resp = requests.get(cocktail_url, params=payload, headers=headers)
+    resp = session.get(cocktail_url, params=payload, headers=headers)
     try:
         cocktails = resp.json()["drinks"]
     except:
