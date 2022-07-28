4731bf9d62716b60aeeeb1a97364d168366dbe55
matwasilewski <mat.p.wasilewski@gmail.com>
2022-07-28 16:10:06 +0100

    extend CLI =

--- src/cocktail_cli/cli.py
+++ src/cocktail_cli/cli.py
@@ -1,9 +1,30 @@
+import os.path
 from typing import List
 
 import click
 
+from cocktail_cli.api import (
+    get_cocktails_that_have_given_ingredients,
+    get_cocktail2ingredients,
+    what_cocktail_can_i_make,
+)
+
 
 @click.command()
-@click.argument('components')
-def cli(components: List[str]):
-    click.echo(f'Components are: {components}!')
+@click.argument("components_path")
+def cli(components_path: str):
+    if not os.path.isfile(components_path):
+        raise FileNotFoundError(
+            f"Path to ingredients {components_path} does not exist!"
+        )
+
+    with open(components_path) as f:
+        ingredients = f.read().split(",")
+
+    if len(cocktails) == 0:
+        click.echo(f"We can't make any cocktails with: {ingredients}!")
+    else:
+
+        click.echo(f"With {ingredients}, we can make:")
+        for cocktail in cocktails:
+            click.echo(cocktail)
