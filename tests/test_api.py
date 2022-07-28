0ab9b6dece50026e81356e7c36a84be00a245f8b
matwasilewski <mat.p.wasilewski@gmail.com>
2022-07-28 13:33:43 +0100

    test for cocktail details

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,13 @@
 import pytest
 import requests
+import json
 
-from src.cocktail_cli.api import cocktails_with_ingredients
+from src.cocktail_cli.api import (
+    cocktails_with_ingredients,
+    cocktail_components,
+)
+
+cocktail_url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007"
 
 
 @pytest.fixture
@@ -13,7 +19,27 @@ def ingredients_1_mock(requests_mock):
     return requests_mock
 
 
+@pytest.fixture
+def cocktail_2_mock(requests_mock):
+    with open("resources/11007_drink_details.json") as f:
+        outcome = json.load(f)
+    requests_mock.get(
+        cocktail_url,
+        json=outcome,
+    )
+    return requests_mock
+
+
 def test_cocktails_from_ingredients_1(ingredients_1_mock):
     ingredients = ["vodka", "tonic", "Anis"]
     cocktails_list = cocktails_with_ingredients(ingredients=ingredients)
     assert len(cocktails_list) == 2
+
+
+def test_cocktail_details(cocktail_2_mock):
+    components_list = cocktail_components(cocktail_url=cocktail_url)
+    assert len(components_list) == 4
+    assert components_list[0] == "Lime juice"
+    assert components_list[1] == "Salt"
+    assert components_list[2] == "Tequila"
+    assert components_list[3] == "Triple sec"
