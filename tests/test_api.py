d6222585f50719316404d2c383a6f4973cc11c9e
matwasilewski <mat.p.wasilewski@gmail.com>
2022-07-28 12:37:21 +0100

    update test

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,9 +1,8 @@
 import requests
 
-from src.cocktail_cli.api import cocktails_from_ingredients
+from src.cocktail_cli.api import cocktails_with_ingredients
 
 
 def test_cocktails_from_ingredients_1():
-    cocktails_list = cocktails_from_ingredients(ingredients=["vodka", "tomato juice", "worcestershire sauce"])
-    assert cocktails_list.status_code == 200
-
+    cocktails_list = cocktails_with_ingredients(ingredients=["vodka", "tonic", "Anis"])
+    assert len(cocktails_list) == 2
