01fd1f46c0485cc75a98ede275123f4498637e09
matwasilewski <mat.p.wasilewski@gmail.com>
2022-07-28 14:33:22 +0100

    add fixtures for cocktails

--- tests/test_api.py
+++ tests/test_api.py
@@ -7,6 +7,7 @@ from src.cocktail_cli.api import (
     cocktail_components,
     cocktail_components_from_id,
     get_cocktails_that_have_given_ingredients,
+    get_cocktail2ingredients,
 )
 
 cocktail_url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11007"
@@ -35,10 +36,43 @@ def blackberry_brandy_mock(requests_mock):
     )
     return requests_mock
 
+@pytest.fixture
+def allegheny_mock(requests_mock):
+    with open("resources/allegheny.json") as f:
+        outcome = json.load(f)
+
+    requests_mock.get(
+        "https://www.thecocktaildb.com/api/json/v1/1/lookup.php\?i\=11021",
+        json=outcome,
+    )
+    return requests_mock
 
 @pytest.fixture
-def cocktail_2_mock(requests_mock):
-    with open("resources/11007_drink_details.json") as f:
+def jelly_bean_mock(requests_mock):
+    with open("resources/turf_cocktail.json") as f:
+        outcome = json.load(f)
+
+    requests_mock.get(
+        "https://www.thecocktaildb.com/api/json/v1/1/lookup.php\?i\=12418",
+        json=outcome,
+    )
+    return requests_mock
+
+@pytest.fixture
+def jelly_bean_mock(requests_mock):
+    with open("resources/jelly_bean.json") as f:
+        outcome = json.load(f)
+
+    requests_mock.get(
+        "https://www.thecocktaildb.com/api/json/v1/1/lookup.php\?i\=13775",
+        json=outcome,
+    )
+    return requests_mock
+
+
+@pytest.fixture
+def margarita_mock(requests_mock):
+    with open("resources/margarita.json") as f:
         outcome = json.load(f)
     requests_mock.get(
         cocktail_url,
@@ -53,7 +87,7 @@ def test_cocktails_from_ingredients_1(anis_mock):
     assert len(cocktails_list) == 2
 
 
-def test_cocktail_details(cocktail_2_mock):
+def test_cocktail_details(margarita_mock):
     components_list = cocktail_components(cocktail_url=cocktail_url)
     assert len(components_list) == 4
     assert components_list[0] == "Lime juice"
@@ -62,7 +96,7 @@ def test_cocktail_details(cocktail_2_mock):
     assert components_list[3] == "Triple sec"
 
 
-def test_cocktail_details_from_id(cocktail_2_mock):
+def test_cocktail_details_from_id(margarita_mock):
     components_list = cocktail_components_from_id("11007")
     assert len(components_list) == 4
     assert components_list[0] == "Lime juice"
@@ -81,3 +115,16 @@ def test_get_cocktails_that_have_given_ingredients(
     assert cocktails[1] == "12418"
     assert cocktails[2] == "13775"
     assert cocktails[3] == "16250"
+
+
+def test_get_cocktails_that_have_given_ingredients():
+    cocktail_ids = ["11021", "12418", "13775", "16250"]
+
+    cocktail2ingredients = get_cocktail2ingredients(cocktail_ids)
+
+    assert len(cocktail2ingredients) == 4
+
+    assert cocktail2ingredients["11021"] == ["Blackberry brandy", "Bourbon", ]
+    assert cocktail2ingredients["12418"] =
+    assert cocktail2ingredients["13775"] =
+    assert cocktail2ingredients["16250"] =
