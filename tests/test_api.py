390e843bb5bc9733b3e4002c417087421d535b04
matwasilewski <mat.p.wasilewski@gmail.com>
2022-07-28 12:51:54 +0100

    move mock to a fixture

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,8 +1,19 @@
+import pytest
 import requests
 
 from src.cocktail_cli.api import cocktails_with_ingredients
 
 
-def test_cocktails_from_ingredients_1():
-    cocktails_list = cocktails_with_ingredients(ingredients=["vodka", "tonic", "Anis"])
+@pytest.fixture
+def ingredients_1_mock(requests_mock):
+    requests_mock.get(
+        "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=vodka&i=tonic&i=Anis",
+        json={"drinks": ["drink1", "drink2"]},
+    )
+    return requests_mock
+
+
+def test_cocktails_from_ingredients_1(ingredients_1_mock):
+    ingredients = ["vodka", "tonic", "Anis"]
+    cocktails_list = cocktails_with_ingredients(ingredients=ingredients)
     assert len(cocktails_list) == 2
